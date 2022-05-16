13a974835b854adec149e14f5af13a8636183a17
AnitaRak <anitatantely@gmail.com>
2022-05-16 10:52:01 +0300

    Update connexion.php

--- Back Office/connexion.php
+++ Back Office/connexion.php
@@ -1,5 +1,5 @@
 <?php
-        /*$host = 'localhost';
+        $host = 'localhost';
         $dbname = 'rechauffement-climatique';
         $username = 'postgres';
         $password = 'tantely10';
@@ -14,8 +14,8 @@
             }
         }catch (PDOException $e){
             echo $e->getMessage();
-        }*/
-        $dsn = 'mysql:host=localhost;dbname=rechauffement_climatique;port=3306;charset=utf8';
+        }
+        /*$dsn = 'mysql:host=localhost;dbname=rechauffement_climatique;port=3306;charset=utf8';
 
         // Création et test de la connexion
 
@@ -29,6 +29,6 @@
         mail('fauxmail@votremail.com', 'PDOException', $exception->getMessage());
         exit('Erreur de connexion à la base de données');
         
-        }
+        }*/
     
 ?>
